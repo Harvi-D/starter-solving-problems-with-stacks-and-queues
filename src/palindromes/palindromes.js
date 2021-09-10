023904b226381860c3464fa46f942e20a25abc77
Dylan Harville <dr.dyloh@gmail.com>
2021-09-09 23:01:31 -0600

    palindrome algorithm passing all tests

--- src/palindromes/palindromes.js
+++ src/palindromes/palindromes.js
@@ -3,6 +3,21 @@ const Stack = require("../lib/stack");
 const isPalindrome = (sentence) => {
   sentence = sentence.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
   // Your solution here
+  let middle = Math.floor(sentence.length / 2);
+  const newStack = new Stack;
+
+  for (let i = 0; i < middle; i++) {
+    newStack.push(sentence[i]);
+  }
+
+  middle += sentence.length % 2 === 0 ? 0 : 1;
+  
+  for (let i = middle; i < sentence.length; i++){
+    if (sentence[i] !== newStack.pop()) {
+      return false;
+    }
+  }
+  return true;
 };
 
 module.exports = isPalindrome;
