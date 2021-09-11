e83cefa9852f0403047de9341f20eb2038ab3016
Dylan Harville <dr.dyloh@gmail.com>
2021-09-11 17:56:31 -0600

    connected_friennds algo passing all tests

--- src/connected_friends/connected_friends.js
+++ src/connected_friends/connected_friends.js
@@ -1,5 +1,35 @@
 const Queue = require("../lib/queue");
 
-const connected = (G, s, r) => {};
+const connected = (graph, startUser, endUser) => {
+    const users = Object.keys(graph);
+
+    if (users.length === 0) {
+        return false;
+    }
+
+    if (startUser === endUser) {
+        return true;
+    }
+
+    const enqueued = [startUser];
+    const discovered = new Queue;
+    discovered.enqueue(startUser);
+
+    while (discovered.first) {
+        const user = discovered.dequeue();
+        const following = graph[user];
+
+        for (const followedUser of following) {
+            if (followedUser === endUser) {
+                return true;
+            }
+            if (!enqueued.includes(followedUser)) {
+                enqueued.push(followedUser);
+                discovered.enqueue(followedUser);
+            }
+        }
+    }
+    return false;
+};
 
 module.exports = connected;
